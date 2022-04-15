d56db2531d1c9f05e9337aedc2b949ec21a2681a
Garrett Downs <garredow@outlook.com>
2022-04-15 09:02:05 -0400

    ci(version): potentially fix version script

--- deploy/version.sh
+++ deploy/version.sh
@@ -2,6 +2,12 @@ VERSION=$1
 
 echo "Applying version ${VERSION} to manifests"
 
-jq ".version = \"${VERSION}\"" public/manifest.webapp > tmp.json && mv tmp.json manifest.webapp
-jq ".b2g_features.version = \"${VERSION}\"" public/manifest.webmanifest > tmp.json && mv tmp.json public/manifest.webmanifest
-jq ".b2g_features.version = \"${VERSION}\"" public/manifest.en-US.webmanifest > tmp.json && mv tmp.json public/manifest.en-US.webmanifest
+cd public
+
+jq ".version = \"${VERSION}\"" manifest.webapp > tmp.json && mv tmp.json manifest.webapp
+jq ".b2g_features.version = \"${VERSION}\"" manifest.webmanifest > tmp.json && mv tmp.json manifest.webmanifest
+jq ".b2g_features.version = \"${VERSION}\"" manifest.en-US.webmanifest > tmp.json && mv tmp.json manifest.en-US.webmanifest
+
+cd -
+
+echo "Done"
