63619149d205ce3d9a13f6e23455b106801228c8
Garrett Downs <garredow@outlook.com>
2022-04-28 04:44:07 -0400

    feat(dashboard): only display times in player view

--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -1,38 +1,41 @@
 <script lang="ts">
+  import { location } from 'svelte-spa-router';
   import { player } from '../stores/player';
   import { formatTime } from '../utils/formatTime';
 </script>
 
-{#if $player.track}
-  <div class="root">
-    <div class="closed">
-      <div class="track">{$player.track.title} - ${$player.track.user.username}</div>
-      <span class="time" style={`color: var(--accent-color);`}
-        >{formatTime($player.currentTime)}</span
-      >
-    </div>
+{#if $player.track && $location.startsWith('/player')}
+  <div class="times">
+    <span class="time" style={`color: var(--accent-color);`}>{formatTime($player.currentTime)}</span
+    >
+    <span class="time" style={`color: var(--accent-color);`}>{formatTime($player.duration)}</span>
+  </div>
+{:else if $player.track}
+  <div class="miniplayer">
+    <div class="track">{$player.track.title} - ${$player.track.user.username}</div>
+    <span class="time" style={`color: var(--accent-color);`}>{formatTime($player.currentTime)}</span
+    >
   </div>
 {/if}
 
 <style>
-  .root {
+  .times {
+    display: flex;
+    justify-content: space-between;
+  }
+  .miniplayer {
     display: flex;
     align-items: center;
     color: rgba(255, 255, 255, 0.88);
     padding: 5px;
   }
-
-  .closed {
-    display: flex;
-    align-items: center;
-    width: 100%;
-  }
-  .closed > .track {
+  .track {
+    flex: 1;
     white-space: nowrap;
     text-overflow: ellipsis;
     overflow: hidden;
   }
-  .closed > .time {
+  .time {
     font-weight: 600;
     margin: 0 3px;
   }
